4726d19c411d23c2f99ea3d7496e202005350582
b5050d <bdyer5280@gmail.com>
2025-05-13 06:52:19 -0600

    Adding working deploy script

---
+++ cd/refresh_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /home/b5050d/Workspace/portfolio || exit 1
+
+git remote update
+
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+BASE=$(git merge-base @ @{u})
+
+if [ "$LOCAL" = "$REMOTE" ]; then
+	echo "Repo up to date"
+elif [ "$LOCAL" = "$BASE" ]; then
+	echo "Pulling changes"
+	git pull
+	sudo systemctl restart portfolio
+elif [ "$REMOTE" = "$BASE" ]; then
+	echo "Local is ahead of remote"
+else
+	echo "Diverged - manual intervention needed"
+fi
